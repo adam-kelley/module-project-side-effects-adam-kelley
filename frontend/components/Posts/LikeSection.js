5ce038130bc61cb4130675ac4ec623ed2a96fb76
Adam Kelley <adkelley1989@icloud.com>
2024-05-13 18:56:03 -0400

    wip

--- frontend/components/Posts/LikeSection.js
+++ frontend/components/Posts/LikeSection.js
@@ -6,6 +6,7 @@ import React from 'react';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
+
   const { likePost, numberOfLikes } = props;
 
   return (
@@ -14,14 +15,14 @@ const LikeSection = props => {
         className='like-section'
         key='likes-icons-container'
       >
-        <div className='like-section-wrapper'>
+        <div onClick = {likePost} className='like-section-wrapper'>
           ❤️
         </div>
         <div className='like-section-wrapper'>
           💬
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
